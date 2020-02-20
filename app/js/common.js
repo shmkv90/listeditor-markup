8856b54c55106e1b1bf70ec733c8fe4b683c3d1d
shmkv <shumakov.oleg1990@gmail.com>
2020-02-20 19:32:20 +0300

    iTooLabs - CustomList control GULP-config

---
+++ app/js/common.js
@@ -0,0 +1,47 @@
+$(function(){
+	
+	$('body').addClass('is-initialised');
+
+
+	// $('.js-catalog-rotator').slick({
+	// 	slidesToShow: 1,
+	// 	slidesToScroll: 1,
+	// 	autoplay: true,
+	// 	autoplaySpeed: 4500,
+	// 	focusOnSelect: true,
+	// 	infinite: true,
+	// 	arrows: false,
+	// 	dots: true,
+	// });
+
+	// let homepageServices = new Swiper ('.js-services-slider .swiper-container', {
+	// 	loop: true,
+	// 	direction: 'horizontal',
+	// 	slidesPerView: 3,
+	// 	spaceBetween: 0,
+	// 	speed: 700,
+	// 	autoplay: {
+	// 		delay: 4000,
+	// 	},
+	// 	navigation: {
+  //     nextEl: '.swiper-button-next',
+  //     prevEl: '.swiper-button-prev',
+	// 	},
+	// 	breakpoints: {
+	// 		1024: {
+	// 			slidesPerView: 2,
+	// 		},
+	// 		480: {
+	// 			slidesPerView: 1,
+	// 		},
+	// 	}
+	// });
+
+
+
+
+
+
+
+});
+
